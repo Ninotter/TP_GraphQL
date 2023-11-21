df135a55b07205ff5582e05e1bfe40fdf6ee73b5
Ninotter <nino.didier@live.fr>
2023-11-21 09:10:30 +0100

    model entities

---
+++ TP_WebServicesGraphQL_Docker/Model/Game.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace TP_WebServicesGraphQL_Docker.Model
+{
+    public class Game
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(128)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public List<string> Genres { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        [Required]
+        public List<Editor> Editors { get; set; } = new List<Editor>();
+
+        [Required]
+        public List<Studio> Studios { get; set; } = new List<Studio>();
+
+        [Required]
+        public List<string> Platforms { get; set; } = new List<string>();
+    }
+}
